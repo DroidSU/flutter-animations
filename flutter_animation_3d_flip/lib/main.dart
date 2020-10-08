d2fb2ca9a5c695ecd47f0c676d0334a89ece11fc
Sujoy Datta <sujoydatta26@gmail.com>
2020-10-09 03:02:12 +0530

    Added basic animation

---
+++ flutter_animation_3d_flip/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animation_3d_flip/custom_drawer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.white,
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomDrawer(),
+    );
+  }
+}
---
+++ flutter_app/android/app/src/main/kotlin/com/morningstar/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.morningstar.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
